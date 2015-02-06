5f11b833b63841fc6e6d4b677b0ec8cc7d25eed0
Luke Scott <luke@webconnex.com>
2015-02-06 14:23:36 -0800

    Add config to optionally add sham

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ var createPattern = function (pattern) {
     };
 };
 
-var initShim = function (files) {
-    files.unshift(createPattern(path.resolve(require.resolve('es5-shim'))));
+var initShim = function (files, es5) {
+    if (es5.sham === true) {
+        files.unshift(createPattern(path.resolve(require.resolve('es5-sham'))));    
+    }
+    if (es5.shim !== false) {
+        files.unshift(createPattern(path.resolve(require.resolve('es5-shim'))));    
+    }
 };
+initShim.$inject = ['config.files', 'config.es5'];
 
-initShim.$inject = ['config.files'];
 module.exports = {
     'framework:es5-shim': ['factory', initShim]
 };
